97bc24c3e52ceb3d4e966c14d661bacb5e5fbb6d
Russu Sasha <russuSasha19@gmail.com>
2017-08-07 18:27:34 +0300

    '05.1'

--- createDb.js
+++ createDb.js
@@ -1,22 +1,20 @@
-var MongoClient = require('mongodb').MongoClient
-  , format = require('util').format;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-MongoClient.connect('mongodb://127.0.0.1:27017/chat', function(err, db) {
-  if (err) throw err;
+var schema = mongoose.Schema({
+  name: String
+});
+schema.methods.meow = function() {
+  console.log(this.get('name'));
+};
 
-  var collection = db.collection('test_insert');
-  collection.remove({}, function(err, affected) {
-    if (err) throw err;
+var Cat = mongoose.model('Cat', schema);
 
-    collection.insert({a: 2}, function(err, docs) {
+var kitty = new Cat({
+  name: 'Zildjian',
+  test: 5
+});
 
-      var cursor = collection.find({a: 2});
-      cursor.toArray(function(err, results) {
-        console.dir(results);
-        // Let's close the db
-        db.close();
-      });
-    });
-
-  });
+kitty.save(function(err, kitty, affected) {
+  kitty.meow();
 });
