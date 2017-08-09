c2294d0cdd069b826aa5a6eafb9141c4b90ae788
Russu Sasha <russuSasha19@gmail.com>
2017-08-09 16:54:16 +0300

    '07'

--- app.js
+++ app.js
@@ -3,52 +3,52 @@ var http = require('http');
 var path = require('path');
 var config = require('config');
 var log = require('libs/log')(module);
+var HttpError = require('error').HttpError;
 
 var app = express();
 
 app.engine('ejs', require('ejs-locals'));
-app.set('views', path.join(__dirname, '/template'));
+app.set('views', __dirname + '/template');
 app.set('view engine', 'ejs');
 
 app.use(express.favicon());
+
 if (app.get('env') == 'development') {
   app.use(express.logger('dev'));
 } else {
   app.use(express.logger('default'));
 }
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.bodyParser());
 
-//app.use(express.session());
+app.use(express.cookieParser());
+
+app.use(require('middleware/sendHttpError'));
 
 app.use(app.router);
 
-app.get('/', function(req, res, next) {
-  res.render("index", {
-
-  });
-});
+require('routes')(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(err, req, res, next) {
-  if (app.get('env') == 'development') {
-    var errorHandler = express.errorHandler();
-    errorHandler(err, req, res, next);
+  if (typeof err == 'number') { // next(404);
+    err = new HttpError(err);
+  }
+
+  if (err instanceof HttpError) {
+    res.sendHttpError(err);
   } else {
-    res.send(500);
+    if (app.get('env') == 'development') {
+      express.errorHandler()(err, req, res, next);
+    } else {
+      log.error(err);
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }
   }
 });
-
-/*var routes = require('./routes');
-var user = require('./routes/user');
-
-// all environments
-
-app.get('/', routes.index);
-app.get('/users', user.list);*/
 
 
 http.createServer(app).listen(config.get('port'), function(){
